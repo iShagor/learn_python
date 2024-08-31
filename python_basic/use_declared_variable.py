046f6f77334f12974f2317544ec7ce2dede3c865
iShagor <bci.shagor.65@gmail.com>
2024-08-31 23:33:58 +0600

    second day learning

---
+++ python_basic/use_declared_variable.py
@@ -0,0 +1,9 @@
+message = "Hello World!!"
+user_name = "iShagor"
+first_name = "Md"
+last_name = "Shagor"
+age = 23
+
+
+print(message, user_name)
+print("Your Name is ", first_name, last_name, ". Age is: ", age)
---
+++ python_basic/variable.py
@@ -0,0 +1,12 @@
+message = "Hello World!!!"
+
+print(message)
+
+user_name = "Sazzat Hossain1"
+
+print(user_name)
+
+
+#variable don't start with number
+#don't use keyword & this is reserve keyword
+#variable using name is miningful
