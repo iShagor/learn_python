64e2f15c8f673431e2105d77c40d90443fef0518
Md Shagor <bci.shagor.65@gmail.com>
2024-09-15 13:24:46 +0600

    for loop use case

---
+++ control_statements/for_loop.py
@@ -0,0 +1,6 @@
+for <var> in <iterable>:
+    <statement(s)>
+
+# <iterable> is a collection of object, for example, a list or tuple.
+# <statement(s)> in the loop body are denoted by indentation.
+# <var> takes on the value of the next element in <iterable> each time through the loop
